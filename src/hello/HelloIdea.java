242ab87bfd21217ae15a5d7298fd399cdb4783de
king_qiaok <wonderful@qq.com>
2018-10-06 16:44:07 +0800

    模块2的提交

--- src/hello/HelloIdea.java
+++ src/hello/HelloIdea.java
@@ -3,5 +3,6 @@ package hello;
 public class HelloIdea {
     public static void main(String[] args) {
         System.out.println("hello word!");
+        System.out.println("hello word!");
     }
 }
