c75dcb113f6315e942f3595c07fc55935bef776f
king_qiaok <wonderful@qq.com>
2018-10-06 17:27:33 +0800

    22222

--- src/hello/Hell02.java
+++ src/hello/Hell02.java
@@ -2,7 +2,6 @@ package hello;
 
 public class Hell02 {
     public static void main(String[] args) {
-        System.out.println("hell0 222 111 333");
-		System.out.println("文件夹的修改");
+        System.out.println("hell0 222 333");
     }
 }
