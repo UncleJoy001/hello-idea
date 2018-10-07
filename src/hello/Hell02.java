340b97fbe425ef6fd523f401408675c2301f8e63
UncleJoy <wonderful@qq.com>
2018-10-07 08:35:35 +0800

    11111111111

--- src/hello/Hell02.java
+++ src/hello/Hell02.java
@@ -2,8 +2,8 @@ package hello;
 
 public class Hell02 {
     public static void main(String[] args) {
-        System.out.println("hell0 222 444 000");
-		System.out.println("hell0 222 444 000");
-		System.out.println("hell0 222 444 000");
+        System.out.println("hell0 222 444 111");
+		System.out.println("hell0 222 444 111");
+		System.out.println("hell0 222 444 111");
     }
 }
